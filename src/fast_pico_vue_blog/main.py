e683f61204ea0a91b985d2bee0c291f6f505a2a5
jrporto <joao6roberto@gmail.com>
2024-02-10 18:57:29 -0300

    Query for user name

--- src/fast_pico_vue_blog/main.py
+++ src/fast_pico_vue_blog/main.py
@@ -1,8 +1,12 @@
 import asyncio
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, Query
+from typing import Annotated
+# from pydantic import 
 
 app = FastAPI()
 
 @app.get("/")
-async def get_root():
-    return ({"Hello": "World"})
+async def get_root(name: Annotated[ str, Query(max_length=30) ] = None):
+    if name:
+        return ({"Hello": f"bobão {name}"})
+    return ("Quem é vc?")
